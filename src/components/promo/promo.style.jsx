f8bc624bbb84e1e68487a18044ee4e62bf846c38
rabbitblood <qz154772610@gmail.com>
2024-05-23 16:55:39 -0700

    update style

--- src/components/promo/promo.style.jsx
+++ src/components/promo/promo.style.jsx
@@ -1,45 +1,51 @@
-import styled, {css, devices, themeGet} from "@styled";
+import styled, { css, devices, themeGet } from "@styled";
 
 export const PromoTitle = styled.h3`
   font-size: 30px;
   line-height: 39px;
   margin-bottom: 16px;
-  color: ${themeGet('colors.heading')};
-  font-weight: ${themeGet('fontWeights.subHeading')};
+  color: ${themeGet("colors.heading")};
+  font-weight: ${themeGet("fontWeights.subHeading")};
 
   ${devices.xs} {
     font-size: 26px;
     line-height: 26px;
   }
-`
+`;
 export const PromoContent = styled.div`
   align-self: center;
-`
+`;
 
 export const PromoInfo = styled.div`
-  position: absolute;
+  position: relative;
   top: 0;
   left: 0;
   display: flex;
   flex-direction: row;
   width: 100%;
   height: 100%;
+  max-height: 100%;
   padding: 40px;
   justify-content: flex-start;
+  text-overflow: ellipsis;
 
   ${devices.xs} {
     padding: 20px;
   }
 
-  ${props => props.align === 'right' && css`
-    justify-content: flex-end;
-  `}
+  ${(props) =>
+    props.align === "right" &&
+    css`
+      justify-content: flex-end;
+    `}
 
-  ${props => props.align === 'center' && css`
-    justify-content: center;
-    text-align: center;
-  `}
-`
+  ${(props) =>
+    props.align === "center" &&
+    css`
+      justify-content: center;
+      text-align: center;
+    `}
+`;
 
 export const PromoItem = styled.a`
   display: block;
@@ -49,6 +55,6 @@ export const PromoItem = styled.a`
 
   img {
     width: 100%;
-    transition: ${themeGet('transition')};
+    transition: ${themeGet("transition")};
   }
-`
+`;
